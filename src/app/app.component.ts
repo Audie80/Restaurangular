bdedc6c2243fd744bbaa6e5f19f933b9691ed214
AUDE VELLY MENUT <aude.velly-menut@arkea.com>
2025-11-26 17:51:14 +0100

    Init Angular project with Material & Navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {MainNavComponent} from './main-nav/main-nav.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'audie-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MainNavComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'restaurangular';
