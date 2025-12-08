d5097057bcd8087a8d628eb7049656c3a64bd911
f1678 <aude.velly-menut@arkea.com>
2025-12-08 10:40:31 +0100

    Test input binding

--- src/app/views/reserve/reserve.component.ts
+++ src/app/views/reserve/reserve.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { JsonPipe, NgFor } from "@angular/common";
 
 @Component({
   selector: 'audie-reserve',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgFor, JsonPipe],
   templateUrl: './reserve.component.html',
-  styleUrl: './reserve.component.css'
+  styleUrl: './reserve.component.css',
 })
-export class ReserveComponent {
+export class ReserveComponent implements OnInit {
+  reservationForm!: FormGroup;
+  date = new Date();
 
+  timeOptions = [
+    '12:00',
+    '12:30',
+    '13:00',
+    '13:30',
+    '19:00',
+    '19:30',
+    '20:00',
+    '20:30',
+  ];
+  peopleOptions = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  constructor(private readonly fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.reservationForm = this.fb.group({
+      date: new Date(),
+      time: '',
+      people: 1,
+    });
+  }
 }
